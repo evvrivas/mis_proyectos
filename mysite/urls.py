44b853ec9de9b22419e472b04c783456f5711999
evvrivas <evvrivas@gmail.com>
2017-12-20 02:17:02 -0600

    du

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,7 +28,7 @@ from mysite.views import *
 
 
 
-urlpatterns = patterns('',
+urlpatterns = ['',
     # Examples:
     # url(r'^$', 'artetronica.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -37,23 +37,23 @@ urlpatterns = patterns('',
      url(r'^$', Index.as_view(), name='index'),
     (r'^accounts/login/$', login,{'template_name': 'login.html'}),
     (r'^accounts/logout/$', logout),
-)
+]
 
 #r'^admin/', include(admin.site.urls)
 
 
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += patterns('',	(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.STATIC_ROOT}))
-urlpatterns += patterns('',(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':  settings.MEDIA_ROOT}))
+urlpatterns += url('',	(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.STATIC_ROOT}))
+urlpatterns += url('',(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':  settings.MEDIA_ROOT}))
 
 urlpatterns += static(settings.MEDIA_ROOT, document_root=settings.MEDIA_URL)
 
 
-urlpatterns += patterns('',(r'^principal/$', pagina_principal),(r'^catalogo/(\d+)$', catalogo),(r'^informacion/$', informacion)) + staticfiles_urlpatterns()
-urlpatterns += patterns('',(r'^listado/([a-z]+)$', listado),(r'^editar/(\d+)/$', editar),)
-urlpatterns += patterns('',(r'^entrada_usuario/$', entrada_usuario),(r'^entrada_mensaje/(\d+)$', entrada_mensaje),(r'^descargar_material/$', descargar_material),)
-urlpatterns += patterns('',(r'^add_to_cart_PCB/(\d+)/([^/]+)/$', add_to_cart_PCB),(r'^get_cart/$', get_cart),)
+urlpatterns += url('',(r'^principal/$', pagina_principal),(r'^catalogo/(\d+)$', catalogo),(r'^informacion/$', informacion)) + staticfiles_urlpatterns()
+urlpatterns += url('',(r'^listado/([a-z]+)$', listado),(r'^editar/(\d+)/$', editar),)
+urlpatterns += url('',(r'^entrada_usuario/$', entrada_usuario),(r'^entrada_mensaje/(\d+)$', entrada_mensaje),(r'^descargar_material/$', descargar_material),)
+urlpatterns += url('',(r'^add_to_cart_PCB/(\d+)/([^/]+)/$', add_to_cart_PCB),(r'^get_cart/$', get_cart),)
 
 
 
