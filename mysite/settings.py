9dee98ae7c927461be9aa45529ce8587466d2f8d
evvrivas <evvrivas@gmail.com>
2017-12-22 00:40:26 -0600

    oladds

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ TEMPLATE_DEBUG = True
 if ON_PAAS:
     ALLOWED_HOSTS = [os.environ['OPENSHIFT_APP_DNS'], socket.gethostname()]
 else:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['*']
 
 # Application definition
 
