f2c85e5d2fb3a4524da776b622513eac995f921a
evvrivas <evvrivas@gmail.com>
2018-01-01 13:44:02 -0600

    thumbnails3

--- mysite/urls.py
+++ mysite/urls.py
@@ -64,6 +64,7 @@ urlpatterns = [
 #r'^admin/', include(admin.site.urls)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #urlpatterns += static(settings.MEDIA_ROOT, document_root=settings.MEDIA_URL)
