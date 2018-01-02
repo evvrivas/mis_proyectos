44d4d28ff57b38e41d1f999fb505dbb79de41c6e
evvrivas <evvrivas@gmail.com>
2018-01-01 19:28:53 -0600

    t

--- mysite/urls.py
+++ mysite/urls.py
@@ -62,16 +62,7 @@ urlpatterns = [
 
 ]
 
-#r'^admin/', include(admin.site.urls)
-
     
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-#urlpatterns += static(settings.MEDIA_ROOT, document_root=settings.MEDIA_URL)
-
-urlpatterns += url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-
-
 
