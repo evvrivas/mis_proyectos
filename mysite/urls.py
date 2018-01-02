6bc122924552ffda422bcddb250b8e2deeaa55eb
evvrivas <evvrivas@gmail.com>
2018-01-01 18:01:41 -0600

    thumbnailsxz

--- mysite/urls.py
+++ mysite/urls.py
@@ -71,8 +71,7 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #urlpatterns += static(settings.MEDIA_ROOT, document_root=settings.MEDIA_URL)
 
- settings.DEBUG :
-    urlpatterns += patterns('',
+urlpatterns += patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     )
 
