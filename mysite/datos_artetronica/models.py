e44dfd22ae10e91e02bf8d669d4f30f8b491be72
evvrivas <evvrivas@gmail.com>
2018-02-25 12:59:01 -0600

    conducta

--- mysite/datos_artetronica/models.py
+++ mysite/datos_artetronica/models.py
@@ -87,7 +87,7 @@ class Productos(models.Model):
 	    
 	     descripcion = models.TextField(max_length=100,blank=True)
 	     puntuacion	 = models.CharField(max_length=30,default=0) 
-	     estado=  models.CharField(max_length=30,choices=ESTADO) 
+	     #estado=  models.CharField(max_length=30,choices=ESTADO) 
 	     precio_A  = models.FloatField(blank=True,null= True	)	     
 	     fecha_ingreso = models.DateField(default=datetime.now,editable = False)
 
@@ -153,9 +153,9 @@ class Tiendas(models.Model):
 	     ubicacion=models.CharField(max_length=30,blank=True)
 	     categoria=models.ForeignKey('Categoria',blank=True,null=True)	     
 	     imagen1 = ImageField(upload_to='tmp')
-	     descripcion=models.CharField(max_length=60,blank=True)
+	     descripcion=models.CharField(max_length=90,blank=True)
 	     
-	     info=models.CharField(max_length=30,choices=INFORMA,blank=True)
+	     #info=models.CharField(max_length=30,choices=INFORMA,blank=True)
 
 	     fecha_ingreso = models.DateField(default=datetime.now,editable = False)
 	     	     
@@ -216,3 +216,63 @@ class Pedidos(models.Model):
 		    		return  self.nombre
 	     class Admin:
 		    		list_display = ('categoria', 'nombre','total')
+
+
+
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+class Cart(models.Model):
+				creation_date = models.DateTimeField(verbose_name=_('creation date'))
+				checked_out = models.BooleanField(default=False, verbose_name=_('checked out'))
+
+				class Meta:
+								verbose_name = _('cart')
+								verbose_name_plural = _('carts')
+								ordering = ('-creation_date',)
+
+				def __unicode__(self):
+								return unicode(self.creation_date)
+
+class ItemManager(models.Manager):
+				def get(self, *args, **kwargs):
+								if 'product' in kwargs:
+												kwargs['content_type'] = ContentType.objects.get_for_model(type(kwargs['product']))
+												kwargs['object_id'] = kwargs['product'].pk
+												del(kwargs['product'])
+								return super(ItemManager, self).get(*args, **kwargs)
+
+class Item(models.Model):
+				cart = models.ForeignKey(Cart, verbose_name=_('cart'))
+				quantity = models.PositiveIntegerField(verbose_name=_('quantity'))
+				unit_price = models.DecimalField(max_digits=18, decimal_places=2, verbose_name=_('unit price'))
+				# product as generic relation
+				content_type = models.ForeignKey(ContentType)
+				object_id = models.PositiveIntegerField()
+
+				objects = ItemManager()
+
+				class Meta:
+								verbose_name = _('item')
+								verbose_name_plural = _('items')
+								ordering = ('cart',)
+
+				def __unicode__(self):
+								return u'%d units of %s' % (self.quantity, self.product.__class__.__name__)
+
+				def total_price(self):
+								return self.quantity * self.unit_price
+				total_price = property(total_price)
+
+				# product
+				def get_product(self):
+								return self.content_type.get_object_for_this_type(pk=self.object_id)
+
+				def set_product(self, product):
+								self.content_type = ContentType.objects.get_for_model(type(product))
+								self.object_id = product.pk
+
+				product = property(get_product, set_product)
