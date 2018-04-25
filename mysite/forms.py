b2f8da7d8db5df759115f31e8ee363988ced9a8e
evvrivas <evvrivas@gmail.com>
2018-04-25 08:31:15 -0600

    fulk

--- mysite/forms.py
+++ mysite/forms.py
@@ -15,7 +15,7 @@ class ProductosForm(ModelForm):
 		widgets = {'descripcion': Textarea(attrs={'cols': 40, 'rows': 3}),}
 		exclude=["id_usuario","puntuacion","fecha_ingreso"]
 	
-	def __init__(self, id_usuario. *args, **kwars):
+	def __init__(self, id_usuario, *args, **kwars):
 		super(ProductosForm, self).__init(*args,**kwargs)
 		self.fields['categoria'].queryset=Category.objects.filter(id_usuario=request.user.username)
 
