463ebf7a2df9768ad09fdb80b234e14773079891
evvrivas <evvrivas@gmail.com>
2018-04-25 10:57:08 -0600

    fulk

--- mysite/forms.py
+++ mysite/forms.py
@@ -12,12 +12,11 @@ class UsuariosForm(ModelForm):
 class ProductosForm(ModelForm):
 	class Meta:
 		model= Productos
+		model.fields['categoria'].queryset=Categoria.objects.filter(id_usuario=request.user.username)
 		widgets = {'descripcion': Textarea(attrs={'cols': 40, 'rows': 3}),}
 		exclude=["id_usuario","puntuacion","fecha_ingreso"]
+	    
 	
-	def __init__(self, id_usuario, *args, **kwars):
-		super(ProductosForm, self).__init(*args,**kwargs)
-		self.fields['categoria'].queryset=Category.objects.filter(id_usuario=request.user.username)
 
 
 class BuscarForm(ModelForm):
--- mysite/views.py
+++ mysite/views.py
@@ -110,7 +110,7 @@ def crear_producto(request,idusuario,nombretienda):
                                              
                                                       
                  else:
-                    form=ProductosForm(id_usuario=request.user.username)
+                    form=ProductosForm()
                     formCateg=CategoriaForm()                       
                     return render(request,'entrada_producto.html',locals())
 
@@ -182,7 +182,7 @@ def editar_producto(request,idusuario,nombretienda,acid):
                                   return render(request,'entrada_producto.html',locals())  
         else:
             
-            form = ProductosForm(instance=f,id_usuario=request.user.username)
+            form = ProductosForm(instance=f)
             formCateg=CategoriaForm()
 
         
