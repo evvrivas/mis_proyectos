8a413f1e2618906fc23a38321e11ce493e694b61
evvrivas <evvrivas@gmail.com>
2018-06-14 10:40:18 -0600

    casi2

--- mysite/datos_artetronica/models.py
+++ mysite/datos_artetronica/models.py
@@ -108,8 +108,8 @@ class Productos(models.Model):
 	     puntuacion	 = models.CharField(max_length=30,default=0) 
 	     #estado=  models.CharField(max_length=30,choices=ESTADO) 
 	     precio_A  = models.FloatField(default=0,blank=True,null=True)
-	     precio_B=models.FloatField(blank=True,null=True)	   
-         estado_prod=models.CharField(max_length=30,blank=True,choices=ESTADO_PRODUCTO,default="EN_EXISTENCIA")     
+	     precio_B=models.FloatField(blank=True,null=True)
+	     estado_prod=models.CharField(max_length=30,blank=True,choices=ESTADO_PRODUCTO,default="EN_EXISTENCIA")
 	     fecha_ingreso = models.DateField(default=datetime.now,editable = False)
 
 	      	    
