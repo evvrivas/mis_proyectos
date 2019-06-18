44e437622cd58d9f5862a70321fee3edacad2380
evvrivas <evvrivas@gmail.com>
2019-06-18 00:11:00 -0600

    Color fondo

--- mysite/views.py
+++ mysite/views.py
@@ -82,7 +82,7 @@ def crear_categorias(request):
   
  
 
-  fechita=atetime.datetime.now()
+  fechita=datetime.datetime.now()
 
   u1=User.objects.create_user(username="50378218224", password="1111",email="evvrivas@gmail.com",first_name="Ernesto",last_name="Valdez")
   u1.save()
