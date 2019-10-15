a0295adf3837bb438577c00fddf2987d94eb824b
evvrivas <evvrivas@gmail.com>
2019-10-15 13:13:54 -0600

    fin

--- mysite/datos_artetronica/models.py
+++ mysite/datos_artetronica/models.py
@@ -235,17 +235,16 @@ class Usuarios(models.Model):
 	     tipo_de_vista=models.CharField(max_length=30,blank=True,default="NORMAL")
 	     tipo_usuario=models.CharField(max_length=30,choices=TIPO_USUARIO,blank=True,default="EL_COMPRADOR")
 	     tipo_vista=models.IntegerField(blank=True,default=0)
-	     def save(self, *args, **kwargs):
-		        if self.image:
-		            small=rescale_image(self.image,width=100,height=100)
-		            self.image_small=SimpleUploadedFile(name,small_pic)
-		        super(Usuarios, self).save(*args, **kwargs)
 	     #def save(self, *args, **kwargs):
-	     	#if not self.pk:
-	     		#super(Usuarios, self).save(*args, **kwargs)
-	     		#resized = get_thumbnail(self.image, "100x100" ,quality=99, format='JPEG')
-	     		#self.image.save(resized.name, ContentFile(resized.read()), True)
-	     	#super(Usuarios, self).save(*args, **kwargs)
+		        #if self.image:
+		            #small=rescale_image(self.image,width=100,height=100)
+		            #self.image_small=SimpleUploadedFile(name,small_pic)
+		        #super(Usuarios, self).save(*args, **kwargs)
+		 def save(self, *args, **kwargs):
+		        if self.image:
+		            	self.image = get_thumbnail(self.image, '100x100', quality=99, format='JPEG')
+		        super(Usuarios, self).save(*args, **kwargs)
+	     
 	     def __str__(self):
 	     		return  self.id_usuario
 	     class Admin:
