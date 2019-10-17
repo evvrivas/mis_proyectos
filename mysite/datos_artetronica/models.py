6a41c0cf3606a335f7893b653edee9cb7b605f94
evvrivas <evvrivas@gmail.com>
2019-10-16 20:33:39 -0600

    usa2

--- mysite/datos_artetronica/models.py
+++ mysite/datos_artetronica/models.py
@@ -242,26 +242,21 @@ class Usuarios(models.Model):
 	     tipo_usuario=models.CharField(max_length=30,choices=TIPO_USUARIO,blank=True,default="EL_COMPRADOR")
 	     tipo_vista=models.IntegerField(blank=True,default=0)
 	     def save(self):
-					#Opening the uploaded image
-					im = Image.open(self.image)
-
-					output = BytesIO()
-
-					#Resize/modify the image
-					im = im.resize( (100,100) )
-
-					#after modifications, save it to the output
-					im.save(output, format='JPEG', quality=100)
-					output.seek(0)
-
-					#change the imagefield value to be the newley modifed image value
-					self.image = InMemoryUploadedFile(output,'ImageField', "%s.jpg" %self.image.name.split('.')[0], 'image/jpeg', sys.getsizeof(output), None)
-
-					super(Modify,self).save()
-		 def __str__(self):
-				     		return  self.id_usuario
-		 class Admin:
-				     		list_display = ('id_usuario')
+	     	#Opening the uploaded image
+	     	im = Image.open(self.image)
+	     	output = BytesIO()
+	     	#Resize/modify the image
+	     	im = im.resize( (100,100) )
+	     	#after modifications, save it to the output
+	     	im.save(output, format='JPEG', quality=100)
+	     	output.seek(0)
+	     	#change the imagefield value to be the newley modifed image value
+	     	self.image = InMemoryUploadedFile(output,'ImageField', "%s.jpg" %self.image.name.split('.')[0], 'image/jpeg', sys.getsizeof(output), None)
+	     	super(Modify,self).save()
+	     def __str__(self):
+	     		return  self.id_usuario
+	     class Admin:
+	     		list_display = ('id_usuario')
 
 
  
