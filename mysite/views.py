10e0c42a18e008d6675078f178686a7b8fda901d
evvrivas <evvrivas@gmail.com>
2019-12-03 23:41:35 -0600

    guis

--- mysite/views.py
+++ mysite/views.py
@@ -1420,7 +1420,7 @@ def ver_el_carrito(request,estado_del_producto):
                           if estado_del_producto=="TODOS":
                               carrito= Carro_de_compras.objects.filter(Q(id_comprador=request.user.username) | Q(delibery=request.user.username)).order_by("producto__tienda__nombre_tienda")
                           elif estado_del_producto=="NUEVO":
-                              carrito= Carro_de_compras.objects..filter(Q(id_comprador=request.user.username) | Q(delibery=request.user.username)).filter( Q(estado_prod='QUIERO_PEDIR_ESTO') |  Q(estado_prod='EL_VENDEDOR_RECIBIO_EL_PEDIDO')).order_by("producto__tienda__nombre_tienda")
+                              carrito= Carro_de_compras.objects.filter(Q(id_comprador=request.user.username) | Q(delibery=request.user.username)).filter( Q(estado_prod='QUIERO_PEDIR_ESTO') |  Q(estado_prod='EL_VENDEDOR_RECIBIO_EL_PEDIDO')).order_by("producto__tienda__nombre_tienda")
                           elif estado_del_producto=="EN_PROCESO":
                               carrito= Carro_de_compras.objects.filter(Q(id_comprador=request.user.username) | Q(delibery=request.user.username)).filter(Q(estado_prod='EL_VENDEDOR_A_CONFIRMADO') | Q(estado_prod='PRODUCTO_ENTREGADO')).order_by("producto__tienda__nombre_tienda")
                           elif estado_del_producto=="FINALIZADO":
