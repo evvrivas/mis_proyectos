6db951362b75318a6696a318843c7aa3aafcce4b
evvrivas <evvrivas@gmail.com>
2020-05-02 21:40:27 -0600

    uno al final

--- mysite/datos_artetronica/models.py
+++ mysite/datos_artetronica/models.py
@@ -242,7 +242,7 @@ class Configuracion_sistema(models.Model):
 			     		t_image.save(output,format='JPEG',quality=75)
 			     		output.seek(0)
 			     		self.image=InMemoryUploadedFile(output,'ImageField',"%s.jpg" %self.image.name,'p_image/jpeg',getsizeof(output),None)
-			     	except:
+			except:
 			     		pass
 	     	super(Tiendas,self).save(*args,**kwargs) 
 	               
